8b0aeea25f14c087d45f024b88e377c5bd997e99
RahulMandyal1 <93306066+RahulMandyal1@users.noreply.github.com>
2022-06-06 17:27:48 +0530

    day three

--- conduitapp/src/components/NewPost.js
+++ conduitapp/src/components/NewPost.js
@@ -1,9 +1,69 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class NewPost extends Component {
+
+  state = {
+    title: "",
+    description: "",
+    body: "",
+    tags: "",
+    errors: {
+      title: "",
+      description: "",
+      body: "",
+      tags: "",
+    },
+  };
+
+  handleChange=({target})=>{
+    const {name , value}  = target;
+    this.setState({
+    [name] : value
+    })
+  }
+
   render() {
     return (
-      <div>NewPost</div>
-    )
+      <section className="newpost-container">
+        <form className="post-container">
+          <div className="form-group">
+            <input
+              placeholder="title"
+              name="title"
+              value={this.state.title}
+              onChange={this.handleChange}
+            />
+            <span className="error"></span>
+          </div>
+          <div className="form-group">
+            <input
+              placeholder="description"
+              name="description"
+              value={this.state.description}
+              onChange={this.handleChange}
+            />
+            <span className="error"></span>
+          </div>
+          <div className="form-group">
+            <textarea
+              placeholder="body"
+              name="body"
+              value={this.state.body}
+              onChange={this.handleChange}
+            />
+            <span className="error"></span>
+          </div>
+          <div className="form-group">
+            <input
+              placeholder="tags"
+              name="tags"
+              value={this.state.tags}
+              onChange={this.handleChange}
+            />
+            <span className="error"></span>
+          </div>
+        </form>
+      </section>
+    );
   }
 }
