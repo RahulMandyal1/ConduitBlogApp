716c2bb52420fbd43dc1f2babd82b9fdc0424d73
RahulMandyal1 <93306066+RahulMandyal1@users.noreply.github.com>
2022-06-23 00:23:01 +0530

    404 not found solved

--- conduitapp/src/index.js
+++ conduitapp/src/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./stylesheets/index.css";
 import App from "./components/App";
 import ErrorBoundary from "./components/ErrorBoundry";
 
 let root = ReactDOM.createRoot(document.querySelector("#root"));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <ErrorBoundary>
       <App />
     </ErrorBoundary>
-  </BrowserRouter>
+  </HashRouter>
 );
