1c827b65fe39caa696c3f47e7ff0f465f460420e
RahulMandyal1 <93306066+RahulMandyal1@users.noreply.github.com>
2022-06-23 10:38:45 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter} from "react-router-dom";
 import "./stylesheets/index.css";
 import App from "./components/App";
 import ErrorBoundary from "./components/ErrorBoundry";
 
 let root = ReactDOM.createRoot(document.querySelector("#root"));
 root.render(
-  <HashRouter>
+  <BrowserRouter>
     <ErrorBoundary>
       <App />
     </ErrorBoundary>
-  </HashRouter>
+  </BrowserRouter>
 );
